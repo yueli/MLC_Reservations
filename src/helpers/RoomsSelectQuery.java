435f8c38e2fb87abf1f396230da08dffaf9eef9e
tunde227 <brian.olaogun@outlook.com>
2016-03-29 01:41:20 -0400

    Fixed Reservation Check Query
    — parenthesis matter.

--- src/helpers/RoomsSelectQuery.java
+++ src/helpers/RoomsSelectQuery.java
@@ -8,7 +8,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import model.Admin;
 import model.DateTimeConverter;
 import model.DbConnect;
 import model.Rooms;
