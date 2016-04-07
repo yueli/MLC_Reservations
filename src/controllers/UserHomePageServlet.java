751358eaacb5667726fa19ffd7feb26599822e8a
tunde227 <brian.olaogun@outlook.com>
2016-04-06 22:00:51 -0400

    Home page user and session checks
    — adding session and user checks to admin and user home pages.

--- src/controllers/UserHomePageServlet.java
+++ src/controllers/UserHomePageServlet.java
@@ -10,6 +10,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import model.DbConnect;
+import model.User;
+
 /**
  * Servlet implementation class UserHomePageServlet
  */
@@ -40,7 +43,35 @@ public class UserHomePageServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		this.session = request.getSession(false);
 		
-		url = "user/userHomePage.jsp";
+		if(this.session != null) {
+			User user = (User) session.getAttribute("user");
+			
+			if(user != null){
+				
+				url = "user/userHomePage.jsp";
+				
+			} else {
+				//------------------------------------------------//
+				/*               USER INFO EXPIRED                */
+				//------------------------------------------------//
+				// if a new session is created with no user object passed
+				// user will need to login again
+				session.invalidate();
+				//url = "LoginServlet"; // USED TO TEST LOCALLY
+				response.sendRedirect(DbConnect.urlRedirect());
+				return;
+			}
+			
+		} else {
+			//------------------------------------------------//
+			/*        INVALID SESSION (SESSION == NULL)       */
+			//------------------------------------------------//
+			// if session has timed out, go to home page
+			// the site should log them out.
+			//url = "LoginServlet";
+			response.sendRedirect(DbConnect.urlRedirect());
+			return;
+		}
 		
 		RequestDispatcher dispatcher = request.getRequestDispatcher(url);
 		dispatcher.forward(request, response);
