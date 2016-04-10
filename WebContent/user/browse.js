285455a024efe253f46c30e8da491c7315143516
xuronnie <ronniex89@gmail.com>
2016-04-10 01:03:58 -0400

    Commit 1
    Commit 1

--- WebContent/user/browse.js
+++
@@ -1,7 +0,0 @@
-function check() {
-	if(document.getElementById('buildingList').value != "")
-		document.getElementById('floorList').disabled = false;
-	else
-		document.getElementById('floorList').disabled = true;
-}
-/* @author Brian Olaogun   Used to show each drop down after the previous is selected */
