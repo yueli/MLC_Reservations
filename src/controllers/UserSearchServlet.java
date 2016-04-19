33180c9a530f505b530916a1e7b313a13ec2b656
Ginger <anotherginger@gmail.com>
2016-04-19 16:23:52 -0400

    changes to test changed back to merge
     - LoginServlet: changed to test locally
    
     - UserSearchServlet: changed to be ‘do post’ and not ‘do get’
    
     - GingerSearchReservationsServlet. GingerSearch.jsp - new to test my
    search - will keep for a little while
    
     - userheader.html: doesn’t like a space I put in there… couldn’t find
    the space it didn’t like
    
     - web.xml: can never seem to please github when trying to fix the
    difference in the spaces

--- src/controllers/UserSearchServlet.java
+++ src/controllers/UserSearchServlet.java
@@ -41,7 +41,7 @@ public class UserSearchServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doPost(request, response);
+		this.doPost(request, response);
 	}
 
 	/**
