a7ff22a3c4af38ce85aa74c4438789f9eb85d9fa
tunde227 <brian.olaogun@outlook.com>
2016-04-20 08:57:55 -0400

    Cleaning Up
    — Cleaning up print lines.  Leaving them in so that it will be easier
    to continue to test.
    
    — Reorganized the names of my time loops so that it will be easier to
    understand my code.

--- src/controllers/UserSearchServlet.java
+++ src/controllers/UserSearchServlet.java
@@ -151,10 +151,10 @@ public class UserSearchServlet extends HttpServlet {
 						// list for the room number.  Below will print all times, inclusive between start and end
 						List<String> roomNumber = rsq.roomList(Integer.parseInt(buildingID));
 						
-						List<String> times = tc.timeRangeList(startTime, TimeConverter.subtractTime("00:00:00", Integer.parseInt(hourIncrement)));
-						List<String> times2 = tc.timeRangeList("00:00:00", TimeConverter.subtractTime(endTime, Integer.parseInt(hourIncrement)));
+						List<String> times = tc.timeRangeList(startTime, TimeConverter.subtractTime(endTime, Integer.parseInt(hourIncrement))); 
+						List<String> times2 = tc.timeRangeList(startTime, TimeConverter.subtractTime("00:00:00", Integer.parseInt(hourIncrement)));
 						List<String> times3 = tc.timeRangeList("00:00:00", TimeConverter.subtractTime("00:00:00", Integer.parseInt(hourIncrement)));
-						List<String> times4 = tc.timeRangeList(startTime, TimeConverter.subtractTime(endTime, Integer.parseInt(hourIncrement))); 
+						List<String> times4 = tc.timeRangeList("00:00:00", TimeConverter.subtractTime(endTime, Integer.parseInt(hourIncrement)));
 						
 						// date range list.  Print all dates between start and end date inclusive
 						List<String> dates = dtc.dateRangeList(startDate, endDate);
@@ -185,12 +185,12 @@ public class UserSearchServlet extends HttpServlet {
 								for (int k = 0; k < dates.size(); k++) {
 									
 									// loop through each time then increment room
-									for (int j =0; j < times4.size(); j++){
+									for (int j =0; j < times.size(); j++){
 										
-										String innerEndTime = TimeConverter.addTime(times4.get(j), Integer.parseInt(hourIncrement));
-										System.out.println("Loop Start time = " + times4.get(j) + " Loop END TIME = " + innerEndTime);
+										String innerEndTime = TimeConverter.addTime(times.get(j), Integer.parseInt(hourIncrement));
+										System.out.println("Loop Start time = " + times.get(j) + " Loop END TIME = " + innerEndTime);
 										
-										List<String> innerTimes = tc.timeRangeList(times4.get(j), innerEndTime);
+										List<String> innerTimes = tc.timeRangeList(times.get(j), innerEndTime);
 										
 										// loop through/display times in range based off hour increment (reservation length)
 										// ex. if hour increment = 1, start time = 9am, end time = 11am --> 9am - 10am, next row = 10am - 11am, etc.
@@ -250,10 +250,11 @@ public class UserSearchServlet extends HttpServlet {
 														// testing - printing to console
 														System.out.println("******* RESERVED ********");
 														System.out.println("RESERVE ID = " + check);
-														System.out.println("DATE " + startDate);
-														System.out.println("END " + endDate);
+														System.out.println("DATE " + dates.get(k));
+														System.out.println("END " + dates.get(k));
 														System.out.println("ROOM NUMBER " + roomNumber.get(i));
-														System.out.println("TIME " + times.get(j));
+														System.out.println("ST TIME " + innerTimes.get(l));
+														System.out.println("END TIME " + innerEndTime);
 														System.out.println("******* RESERVED ********");
 														System.out.println();
 														
@@ -292,14 +293,13 @@ public class UserSearchServlet extends HttpServlet {
 									System.out.println("*************************");
 									if(dates.get(k).equals(startDate)){
 										
-										//List<String> t1 = tc.timeRangeList(startTime, TimeConverter.subtractTime("00:00:00", Integer.parseInt(hourIncrement)));
 										// loop through each time then increment room
-										for (int j =0; j < times.size(); j++){
+										for (int j =0; j < times2.size(); j++){
 											
-											String innerEndTime = TimeConverter.addTime(times.get(j), Integer.parseInt(hourIncrement));
-											System.out.println("Times 1 --> Loop Start time = " + times.get(j) + " Loop END TIME = " + innerEndTime);
+											String innerEndTime = TimeConverter.addTime(times2.get(j), Integer.parseInt(hourIncrement));
+											System.out.println("Times 1: Start Date --> Loop Start time = " + times2.get(j) + " Loop END TIME = " + innerEndTime);
 											
-											List<String> innerTimes = tc.timeRangeList(times.get(j), innerEndTime);
+											List<String> innerTimes = tc.timeRangeList(times2.get(j), innerEndTime);
 											
 											// loop through/display times in range based off hour increment (reservation length)
 											// ex. if hour increment = 1, start time = 9am, end time = 11am --> 9am - 10am, next row = 10am - 11am, etc.
@@ -363,10 +363,11 @@ public class UserSearchServlet extends HttpServlet {
 															// testing - printing to console
 															System.out.println("******* RESERVED ********");
 															System.out.println("RESERVE ID = " + check);
-															System.out.println("DATE " + startDate);
-															System.out.println("END " + endDate);
+															System.out.println("DATE " + dates.get(k));
+															System.out.println("END " + dates.get(k));
 															System.out.println("ROOM NUMBER " + roomNumber.get(i));
-															System.out.println("TIME " + times.get(j));
+															System.out.println("ST TIME " + innerTimes.get(l));
+															System.out.println("END TIME " + innerEndTime);
 															System.out.println("******* RESERVED ********");
 															System.out.println();
 															
@@ -396,7 +397,7 @@ public class UserSearchServlet extends HttpServlet {
 										for (int j =0; j < times3.size(); j++){
 											
 											String innerEndTime = TimeConverter.addTime(times3.get(j), Integer.parseInt(hourIncrement));
-											System.out.println("Times 3 --> Loop Start time = " + times3.get(j) + " Loop END TIME = " + innerEndTime);
+											System.out.println("Times 2: Middle Date --> Loop Start time = " + times3.get(j) + " Loop END TIME = " + innerEndTime);
 											
 											List<String> innerTimes = tc.timeRangeList(times3.get(j), innerEndTime);
 											
@@ -462,10 +463,11 @@ public class UserSearchServlet extends HttpServlet {
 															// testing - printing to console
 															System.out.println("******* RESERVED ********");
 															System.out.println("RESERVE ID = " + check);
-															System.out.println("DATE " + startDate);
-															System.out.println("END " + endDate);
+															System.out.println("DATE " + dates.get(k));
+															System.out.println("END " + dates.get(k));
 															System.out.println("ROOM NUMBER " + roomNumber.get(i));
-															System.out.println("TIME " + times3.get(j));
+															System.out.println("ST TIME " + innerTimes.get(l));
+															System.out.println("END TIME " + innerEndTime);
 															System.out.println("******* RESERVED ********");
 															System.out.println();
 															
@@ -492,12 +494,12 @@ public class UserSearchServlet extends HttpServlet {
 									if (dates.get(k).equals(endDate)){
 										
 										// loop through each time then increment room
-										for (int j =0; j < times2.size(); j++){
+										for (int j =0; j < times4.size(); j++){
 											
-											String innerEndTime = TimeConverter.addTime(times2.get(j), Integer.parseInt(hourIncrement));
-											System.out.println("Times 2 --> Loop Start time = " + times2.get(j) + " Loop END TIME = " + innerEndTime);
+											String innerEndTime = TimeConverter.addTime(times4.get(j), Integer.parseInt(hourIncrement));
+											System.out.println("Times 3: End Date --> Loop Start time = " + times4.get(j) + " Loop END TIME = " + innerEndTime);
 											
-											List<String> innerTimes = tc.timeRangeList(times2.get(j), innerEndTime);
+											List<String> innerTimes = tc.timeRangeList(times4.get(j), innerEndTime);
 											
 											// loop through/display times in range based off hour increment (reservation length)
 											// ex. if hour increment = 1, start time = 9am, end time = 11am --> 9am - 10am, next row = 10am - 11am, etc.
@@ -561,10 +563,11 @@ public class UserSearchServlet extends HttpServlet {
 															// testing - printing to console
 															System.out.println("******* RESERVED ********");
 															System.out.println("RESERVE ID = " + check);
-															System.out.println("DATE " + startDate);
-															System.out.println("END " + endDate);
+															System.out.println("DATE " + dates.get(k));
+															System.out.println("END " + dates.get(k));
 															System.out.println("ROOM NUMBER " + roomNumber.get(i));
-															System.out.println("TIME " + times2.get(j));
+															System.out.println("ST TIME " + innerTimes.get(l));
+															System.out.println("END TIME " + innerEndTime);
 															System.out.println("******* RESERVED ********");
 															System.out.println();
 															
